a639e4bb31166453a55a5c958c9b00a624644ddc
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2025-09-18 11:37:22 +0300

    Implementation that uses readv()

--- files.c
+++ files.c
@@ -3,8 +3,10 @@
 #include <unistd.h>
 #include <linux/io_uring.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <sys/uio.h>
 #include <linux/fs.h>
 
 
@@ -28,6 +30,48 @@ off_t get_file_size(int fd) {
 	} else if (S_ISREG(st.st_mode))
 		return st.st_size;
 	return -1;
+}
+
+void output_buf(const char *buf, size_t len) {
+	while (len--)
+		fputc(*buf++, stdout);
+}
+long min(long a, long b) {
+	return a < b ? a : b;
+}
+int read_and_print_file_readv(char *filename) {
+	int fd = open(filename, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Couldn't open the file %s\n", filename);
+		return 1;
+	}
+	off_t filesize = get_file_size(fd);
+	if (filesize <= 0)
+		return -1;
+#define BLKSIZE 4096
+	int block_cnt = (int)(filesize / BLKSIZE);
+	if (filesize % BLKSIZE)
+		block_cnt++;
+
+	struct iovec iovecs[block_cnt];
+	// Now let's create the iovecs
+	for (ssize_t i = 0, bytes_remaining = filesize; i < block_cnt; i++, bytes_remaining -= BLKSIZE) {
+		long blksize = min(BLKSIZE, bytes_remaining);
+		void *buf = malloc(blksize);
+		iovecs[i].iov_base = buf;
+		iovecs[i].iov_len = blksize;
+	}
+
+	ssize_t error = 0;
+	if ((error = readv(fd, iovecs, block_cnt)) < 0) {
+		fprintf(stderr, "Couldn't readv(): %ld\n", error);
+		return 1;
+	}
+	for (int i = 0; i < block_cnt; i++) {
+		output_buf(iovecs[i].iov_base, iovecs->iov_len);
+		free(iovecs[i].iov_base);
+	}
+	return 0;
 }
 
 // TODO:
@@ -125,6 +169,8 @@ int main(void) {
 	if (fd < 0)
 		printf("Couldn't open the file: %d\n", fd);
 	printf("Bytes in the file: %llu\n", get_file_size(fd));
+	read_and_print_file_readv(filename);
+	close(fd);
 	// if ((errorCode = writesync_hi()))
 		// return errorCode;
 	// if ((errorCode = readsync_lo()))
